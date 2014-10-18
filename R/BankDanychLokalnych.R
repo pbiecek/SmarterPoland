5be1dfdc1d7d546332b1a2cfd412f7ff4f7d4dc2
pbiecek <przemyslaw.biecek@gmail.com>
2014-10-19 01:03:10 +0200

    BDL series

--- R/BankDanychLokalnych.R
+++ R/BankDanychLokalnych.R
@@ -36,3 +36,43 @@ getBDLsearch <- function(query = "", debug = 0, raw = FALSE) {
   do.call(what = rbind, dgs)
 }
 
+
+getBDLseries <- function(metric_id = "", 
+                         slice = NULL,
+                         time_range = NULL,
+                         wojewodztwo_id = NULL,
+                         powiat_id = NULL,
+                         gmina_id = NULL,
+                         meta = NULL,
+                         debug = 0, raw = FALSE) {
+  library(rjson)
+  library(htmltools)
+  
+  url <- paste0('http://api.mojepanstwo.pl:80/bdl/series?metric_id=', metric_id)
+  if (!is.null(slice)) url <- paste0(url, "&slice=", htmlEscape(slice))
+  if (!is.null(time_range)) url <- paste0(url, "&time_range=", htmlEscape(time_range))
+  if (!is.null(wojewodztwo_id)) url <- paste0(url, "&wojewodztwo_id=", htmlEscape(wojewodztwo_id))
+  if (!is.null(powiat_id)) url <- paste0(url, "&powiat_id=", htmlEscape(powiat_id))
+  if (!is.null(gmina_id)) url <- paste0(url, "&gmina_id=", htmlEscape(gmina_id))
+  if (!is.null(meta)) url <- paste0(url, "&meta=", htmlEscape(meta))
+  
+  document <- fromJSON(file=htmlEscape(url), method='C')
+  if (raw) return(document)
+  
+  met <- t(sapply(document$series, function(s) s$slice))
+  fullmet <- do.call(what = rbind, lapply(1:ncol(met), function(d) {
+    tmp <- do.call(what = rbind, document$meta$dimensions[[d]]$options)
+    rownames(tmp) <- tmp[,1]
+    met[,d] <<- unlist(tmp[met[,d],"value"])
+    data.frame(dim=d, tmp)
+  }) )
+    
+  dgs <- lapply(seq_along(document$series), function(sn) {
+    s <- document$series[[sn]]
+    tmp <- data.frame(do.call(what = rbind, args = s$series), units = s$units)
+    for (i in ncol(met):1)
+      tmp <- data.frame(dimension = met[sn,i], tmp)
+    tmp
+  })
+  do.call(what = rbind, dgs)
+}
