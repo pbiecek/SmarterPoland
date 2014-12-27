ad55e47517aaca869c15e5c47cd67b82a7577974
pbiecek <przemyslaw.biecek@gmail.com>
2014-12-27 21:19:25 +0100

    fix for #3

--- R/setEurostatTOC.R
+++ R/setEurostatTOC.R
@@ -1,7 +1,7 @@
 setEurostatTOC <-
 function() {
    if (!exists(".eurostatTOC", envir = .SmarterPolandEnv)) {
-   .eurostatTOC <-  read.table("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=table_of_contents_en.txt",  sep="\t", header=T,  quote="\"", fill = TRUE, comment.char="")
+   .eurostatTOC <-  read.table("http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=table_of_contents_en.txt",  sep="\t", header=T,  quote="\"", fill = TRUE, comment.char="")
     assign(".eurostatTOC", .eurostatTOC, envir = .SmarterPolandEnv)
   }
   invisible(0)
