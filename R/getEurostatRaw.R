93855654303476f7041c6bb7a08357be803fe30e
pbiecek <przemyslaw.biecek@gmail.com>
2014-12-28 17:27:17 +0100

    improvement as in #4

--- R/getEurostatRaw.R
+++ R/getEurostatRaw.R
@@ -1,10 +1,10 @@
 getEurostatRaw <-
-function(kod = "educ_iste", rowRegExp=NULL, colRegExp=NULL) {
+function(kod = "educ_iste", rowRegExp=NULL, colRegExp=NULL, strip.white = TRUE) {
   adres <- paste("http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F",kod,".tsv.gz",sep="")
   tfile <- tempfile()
 #  download and read file
   download.file(adres, tfile)
-  dat <- read.table(gzfile(tfile), sep="\t", na.strings = ": ", header=FALSE, stringsAsFactors=FALSE)
+  dat <- read.table(gzfile(tfile), sep="\t", na.strings = ": ", header=FALSE, stringsAsFactors=FALSE, strip.white = strip.white)
   unlink(tfile)
   colnames(dat) <- as.character(dat[1,])
   dat <- dat[-1,]
