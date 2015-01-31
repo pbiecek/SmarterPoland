323a9195271109add4d348a458ca2078d225768c
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-31 20:07:54 +0100

    update as suggested by MBE

--- R/getMillwardBrown.R
+++ R/getMillwardBrown.R
@@ -11,27 +11,57 @@ get_millwardbrown_detail <- function(x) {
   return(doc)
 }
 
+# getMillwardBrown <- function() {
+#   main_url <- 'http://wybory.millwardbrown.com/partie-polityczne-parlament-krajowy.json'
+#   
+#   ## dane z wykresu
+#   dane <- get_millwardbrown(main_url)
+#   
+#   ## dodatkowe informacje
+#   ids <- sapply(dane$polls, function(x) x$id)
+#   pojedyncze <- lapply(ids,get_millwardbrown_detail)
+#   
+#   # to the data frame
+#   namess <- sapply(dane$data, function(x) x$name)
+#   inds <- which(sapply(namess, length) > 0)
+#   partie <- unlist(namess[inds])
+#   lista <- lapply(inds, function(ind) {
+# #    tmp <- dane$data[[id]]$data
+#     tmp <- as.data.frame(t(as.data.frame(dane$data[[ind]]$data)))
+#     rownames(tmp) <- NULL
+#     colnames(tmp) <- c("data", "poparcie")
+#     tmp$partia <- namess[ind]
+#     tmp
+#   })
+#   do.call(rbind, lista)
+# }
+
 getMillwardBrown <- function() {
   main_url <- 'http://wybory.millwardbrown.com/partie-polityczne-parlament-krajowy.json'
   
-  ## dane z wykresu
-  dane <- get_millwardbrown(main_url)
+  main_data <- get_millwardbrown(main_url)
   
-  ## dodatkowe informacje
-  ids <- sapply(dane$polls, function(x) x$id)
-  pojedyncze <- lapply(ids,get_millwardbrown_detail)
+  ids <- sapply(main_data$polls, function(x) x$id)
+  poll_details <- lapply(ids, get_millwardbrown_detail)
   
-  # to the data frame
-  namess <- sapply(dane$data, function(x) x$name)
-  inds <- which(sapply(namess, length) > 0)
-  partie <- unlist(namess[inds])
-  lista <- lapply(inds, function(ind) {
-#    tmp <- dane$data[[id]]$data
-    tmp <- as.data.frame(t(as.data.frame(dane$data[[ind]]$data)))
-    rownames(tmp) <- NULL
-    colnames(tmp) <- c("data", "poparcie")
-    tmp$partia <- namess[ind]
-    tmp
-  })
-  do.call(rbind, lista)
+  poll_desc <- do.call(rbind,
+            lapply(poll_details,function(x) data.frame(ID = x$id,
+                                    collected_on = as.character(x$collectedOn),
+                                    sample_size = x$sampleSize,
+                                    turnout = x$turnout)))
+  
+  poll_desc_2 <- do.call(rbind,
+            lapply(poll_details,function(x) {
+            data.frame(
+              do.call(rbind,
+                    lapply(x$results, function(z) {
+                    data.frame(value = z$y, name = z$name)
+                  })),
+              ID = x$id) 
+          }))
+  
+  poll_full_details <- merge(poll_desc, poll_desc_2, by="ID", all.y = TRUE)
+  
+  return(poll_full_details)
 }
+
